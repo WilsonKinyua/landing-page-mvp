72082ee88490dc9cc52b6ae5db3596d2672e2f47
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-08-30 16:10:31 +0300

    smooth scroll down along the page

--- js/custom.js
+++ js/custom.js
@@ -504,4 +504,15 @@
 		stickyHeader();
 	});
 
+
+	document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+		anchor.addEventListener('click', function (e) {
+			e.preventDefault();
+	
+			document.querySelector(this.getAttribute('href')).scrollIntoView({
+				behavior: 'smooth'
+			});
+		});
+	});
+	
 })(jQuery);
